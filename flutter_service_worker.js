2592dd4dfb8cd993fe5aba21ac4dcffd55b31d77
Belodubrovsky Vitaly <vitaly@imageteq.com>
2026-03-27 02:36:02 +0300

    Telegram script with defer

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -22,14 +22,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9
 "favicon.ico": "5622804b46b8a8f41fa03b15f2f5fece",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "eabbe0a75416b102e6077df131ae22da",
+"flutter_bootstrap.js": "43616d5a0c4671f16632fb745212e73e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "43758c5f38831369e5aafdd57b206a0d",
 "/": "43758c5f38831369e5aafdd57b206a0d",
-"main.dart.js": "beadc74114e7f5b74e46512a7a084c1b",
+"main.dart.js": "9e0273c25128eaa9e6b19364926274f1",
 "manifest.json": "8ffdee6d1388b25d01a7081e35a98c43",
 "version.json": "50e57fa7564dc134ed565a32bc9e2c55"};
 // The application shell files that are downloaded before a service worker can
